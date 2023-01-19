34661c4fb920e78c26c709a7641de60bd6ac2230
PinoMacri <pinonegozio1@outlook.it>
2023-01-19 16:03:46 +0100

    Esercizio Consegnato

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppTitle from "./components/AppTitle.vue"
+export default {
+
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    AppTitle: AppTitle
+  }
+
+}
+</script>
+
+<template>
+  <AppTitle />
+</template>
+
+<style>
+
+</style>
